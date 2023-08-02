8b9c5ff29f9f348e24d5388cb202d8c1233b22b5
BigBraim <79224234+BigBraim@users.noreply.github.com>
2023-08-02 11:01:00 -0300

    First commit

---
+++ script bhaskara exercicio3.py
@@ -0,0 +1,22 @@
+print ('Escreva os valores a, b, c ')
+a = int( input ('Escreva os valor de a:'))
+b = int( input ('Escreva os valor de b:'))
+c = int( input ('Escreva os valor de c:'))
+
+delta = b**2 - 4*a*c
+x1 = (((-1)*b) + (delta**0.5))/(2*a)
+x2 = (((-1)*b) - (delta**0.5))/(2*a)
+
+if delta == 0:
+    print ('x1 = x2 =', x1)
+
+elif delta>0:
+    print('x1: ', x1)
+    print('X2: ', x2)
+    print('Delta: ', delta)
+    
+else:
+    print('Raiz nao existe')
+
+
+
---
+++ script nome, dia, mes, ano exercicio 2.py
@@ -0,0 +1,7 @@
+nome = input ('Digite seu nome:')
+dia = input ('Digite o dia da sua data de nascimento:')
+mes = input ('Digite o mes da sua data de nascimento:')
+ano = input ('Digite o ano da sua data de nascimento:')
+
+print ('Nome do usuário:',nome)
+print ('Data de nascimento do usuário:',dia,'/',mes,'/',ano)
